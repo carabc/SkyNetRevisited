ff6926d4d70c0f49367a31163589f6659c704287
carabc <bradleycaravana@gmail.com>
2020-05-06 17:40:05 -0400

    Website finished. Ready to be added to portfolio.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,26 @@
+// Activate Nav Resize On Window Scroll
+// Variables:
+const nav = document.querySelector("#main-nav");
+const navHeight = nav.offsetHeight;
+const brand = document.querySelector(".nav-brand");
+const navItems = document.querySelectorAll(".nav-link");
+
+window.onscroll = function () {
+  this.stickyNav();
+};
+
+function stickyNav() {
+  if (window.scrollY > navHeight) {
+    nav.style.padding = "0px";
+    nav.style.backgroundColor = "rgb(71, 108, 151)";
+    nav.style.boxShadow = "0px 0px 10px 0px black";
+  } else {
+    nav.style.padding = "10px";
+    nav.style.backgroundColor = "rgb(71, 108, 151, 0.308)";
+    nav.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)";
+  }
+}
+
 // Activating Responsive Navigation Button
 // Variables:
 const navList = document.querySelector(".responsive-nav");
@@ -21,23 +44,17 @@ function revealMenu() {
   }
 }
 
-// Activate Nav Resize On Window Scroll
-// Variables:
-const nav = document.querySelector("#main-nav");
-const navHeight = nav.offsetHeight;
-const brand = document.querySelector(".nav-brand");
-const navItems = document.querySelectorAll(".nav-link");
+// Activate Smooth Scroll With jQuery
+$("#main-nav a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+    const hash = this.hash;
 
-window.onscroll = function () {
-  this.stickyNav();
-};
-
-function stickyNav() {
-  if (window.scrollY > navHeight) {
-    nav.style.padding = "0px";
-    nav.style.backgroundColor = "rgb(71, 108, 151)";
-  } else {
-    nav.style.padding = "10px";
-    nav.style.backgroundColor = "rgb(71, 108, 151, 0.308)";
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800
+    );
   }
-}
+});
